f7bc56abc2f2ff43b21e15457feb079464a8977f
patokkkbr <87152156+patokkkbr@users.noreply.github.com>
2025-11-14 00:43:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/ARG_OP/',   // 👈 ADICIONE ISTO AQUI
       server: {
         port: 3000,
         host: '0.0.0.0',
